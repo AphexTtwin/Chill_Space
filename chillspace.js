8bb6f787c6846ad991016822a32e5fc0bce10952
Hamza Allaoui <haallaou@c1r8p3.1337.ma>
2026-09-18 02:36:57 +0100

    some fixes

--- chillspace.js
+++ chillspace.js
@@ -1,6 +1,7 @@
 const form = document.querySelector("form");
 const ideaInput = document.querySelector("#idea");
-const plansList = document.querySelector("#plans-list");
+const descriptionInput = document.querySelector("#idea-description");
+const ideasBody = document.querySelector("#ideas-body");
 
 form.addEventListener("submit", function (event) {
 	event.preventDefault();
@@ -9,10 +10,62 @@ form.addEventListener("submit", function (event) {
 	if (idea === "") {
 		return;
 	}
+	const description = descriptionInput.value.trim();
+	console.log(description);
 
-	const planItem = document.createElement("li");
-	planItem.textContent = idea;
-	plansList.append(planItem);
+	// const planItem = document.createElement("li");
+	// planItem.textContent = idea;
+	// ideasBody.append(planItem);
+
+	const ideaRow = document.createElement("tr");
+	const ideaCell = document.createElement("td");
+
+	// ideaCell.textContent = idea;
+	// ideaRow.append(ideaCell);
+
+	const ideaDetails = document.createElement("details");
+	const ideaTitel = document.createElement("summary");
+	const ideaDescription = document.createElement("p");
+
+	ideaTitel.textContent = idea;
+	ideaDescription.textContent = description || "No details added.";
+
+	ideaDetails.append(ideaTitel, ideaDescription);
+
+	const planButton = document.createElement("button");
+	planButton.type = "button";
+	planButton.textContent = "Plan it";
+	ideaDetails.append(planButton);
+
+	ideaCell.append(ideaDetails);
+
+	const interestedCell = document.createElement("tr");
+
+	interestedCell.textContent = "0";
+
+	const actionCell = document.createElement("td");
+	const interestedButton = document.createElement("button");
+	interestedButton.type = "button";
+	interestedButton.textContent = "I'm intrested";
+	actionCell.append(interestedButton);
+
+	let isIntrested = false;
+	interestedButton.addEventListener("click", function() {
+		if (isIntrested)
+		{
+			isIntrested = false;
+			interestedCell.textContent = "0";
+			interestedButton.textContent = "I'm intrested";
+		}
+		else {
+			isIntrested = true;
+			interestedCell.textContent = "1";
+			interestedButton.textContent = "Undo interest";
+		}
+	});
+
+	ideaRow.append(ideaCell, interestedCell, actionCell);
+	ideasBody.append(ideaRow);
 
 	form.reset();
 	ideaInput.focus();
